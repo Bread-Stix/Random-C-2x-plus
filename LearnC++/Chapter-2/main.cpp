843b910345bae2db5c2cfe9963f70b64f66d4ac2
Bread_Stix <89912515+Bread-Stix@users.noreply.github.com>
2026-07-11 23:19:04 +0800

    Add files via upload

---
+++ LearnC++/Chapter-2/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+    /*to Superman after watching the movie last year
+    This is just me writing functions with the idea
+    that Luthor is yelling 1A to punch superman.
+    */
+
+void attackSuperman()
+{
+    std::cout << "1A-! 1A-!\n";
+}
+
+int main()
+{
+
+    std::cout << "Lex Luthor is beating Superman!\n";
+
+    attackSuperman();
+    attackSuperman();
+    attackSuperman();
+
+    std::cout << "Superman! Get up and fight!\n";
+
+
+    return 0;
+}
